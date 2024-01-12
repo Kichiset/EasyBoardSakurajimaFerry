d3df0aea075aec3fdd3eb3655b4f8db4390e50ff
kichiset <kichiset@gmail.com>
2024-01-12 15:09:54 +0900

    バージョンコードを更新 一時退避したnodemodulesとAndroidフォルダを復帰

--- App.js
+++ App.js
@@ -6,17 +6,43 @@ import * as Notifications from 'expo-notifications';
 
 import MainScreen from './screens/MainScreen';
 import KagoshimaDepartureScreen from './screens/KagoshimaDepartureScreen';
-import KagoshimaAll from './screens/KagoshimaAll';
 import SakurajimaDepartureScreen from './screens/SakurajimaDepartureScreen';
-import SakurajimaAll from './screens/SakurajimaAll';
 import Notification from './screens/Notification';
 
+import { AdsConsent , AdsConsentDebugGeography, AdsConsentStatus} from 'react-native-google-mobile-ads';
+
 const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   useEffect(() => {
     requestPermissionsAsync();
   })
+
+  const [nonPersonalizedOnly, setNonPersonalizedOnly] = useState(true);
+
+  useEffect(() => {
+    AdsConsent.requestInfoUpdate({
+      debugGeography: AdsConsentDebugGeography.EEA,
+      testDeviceIdentifiers: ["TEST-DEVICE-HASHED-ID"],
+    }).then(async (consentInfo) => {
+      let status = consentInfo.status;
+      if (
+        consentInfo.isConsentFormAvailable &&
+        status === AdsConsentStatus.REQUIRED
+      ) {
+        const result = await AdsConsent.showForm();
+        status = result.status;
+      }
+
+      if (
+        consentInfo.status === AdsConsentStatus.OBTAINED ||
+        status === AdsConsentStatus.OBTAINED
+      ) {
+        setNonPersonalizedOnly(false);
+      }
+    });
+  }, []);
 
   return (
     <NavigationContainer>
@@ -24,17 +50,26 @@ export default function App() {
         <Stack.Screen name="Main Page" component={MainScreen} />
         <Stack.Screen name="Go to Sakurajima(From Kagoshima)" component={KagoshimaDepartureScreen} />
         <Stack.Screen name="Back to Kagoshima(From Sakurajima)" component={SakurajimaDepartureScreen} />
-        <Stack.Screen name="KagoshimaAll" component={KagoshimaAll} />
-        <Stack.Screen name="SakurajimaAll" component={SakurajimaAll} />
         <Stack.Screen name="Notification" component={Notification} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+const scheduleNotificationAsync = async () => {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      body: 'test',
+    },
+    trigger: {
+      seconds: 5,
+    }
+  })
+}
+
 const requestPermissionsAsync = async () => {
   const { granted } = await Notifications.getPermissionsAsync();
-  //if (granted) { return }
+  if (granted) { return }
 
   await Notifications.requestPermissionsAsync();
 }
@@ -48,3 +83,5 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
   },
 });
+
+
